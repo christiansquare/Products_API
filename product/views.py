a2e8f01e1627bc848dcad0335e9b617b655a2ee6
Christian Square <christiansquare20@gmail.com>
2022-11-16 16:42:53 -0600

    created django animd and password + created objects for the database.

--- product/views.py
+++ product/views.py
@@ -1,8 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializer import Productserializer
+from .models import Product
 
 @api_view(['GET'])
 def product_list(request):
 
+    product = Product.objects.all()
 
-    return Response('ok')
+    serializer = Productserializer(product, many=True)
+
+    return Response(serializer.data)
